41e623e0888862234775a64aedaf5a0dec80ec0c
afkbeto <qv.nguyen00@gmail.com>
2025-02-02 15:57:05 +0100

    feat: add command submodule

---
+++ src/events/onReady.ts
@@ -0,0 +1,23 @@
+import { Client, REST, Routes, TextChannel } from 'discord.js';
+import { config } from '../../config/config.ts';
+import { botDevId, looperId } from '../../config/channels.ts';
+
+const rest = new REST().setToken(config.bot_token);
+
+export async function onReady(client: Client): Promise<void> {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(client.user?.id || 'missing_id', config.guild_id),
+			{ body: [] },
+		);
+
+		console.log('Discord ready!');
+		if (config.environment === 'production') {
+			await (client.channels.cache.get(looperId) as TextChannel).send(`${client.user} has started another loop!`);
+		} else {
+			await (client.channels.cache.get(botDevId) as TextChannel).send(`${client.user} has started another experiment!`);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
