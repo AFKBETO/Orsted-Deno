d4d68fc1b5841c9798714268d27011e55df7e629
afkbeto <qv.nguyen00@gmail.com>
2025-02-02 18:52:33 +0100

    chore: fix move into function instead of global const

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -2,10 +2,10 @@ import { Client, REST, Routes, TextChannel } from 'discord.js';
 import { config } from '../../config/config.ts';
 import { botDevId, looperId } from '../../config/channels.ts';
 
-const rest = new REST().setToken(config.bot_token);
 
 export async function onReady(client: Client): Promise<void> {
 	try {
+		const rest = new REST().setToken(config.bot_token);
 		await rest.put(
 			Routes.applicationGuildCommands(client.user?.id || 'missing_id', config.guild_id),
 			{ body: [] },
