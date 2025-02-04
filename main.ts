103df86c27ac26d423d874289a92e54f27a3cb71
afkbeto <qv.nguyen00@gmail.com>
2025-02-04 13:57:13 +0100

    feat: add mongoose

--- main.ts
+++ main.ts
@@ -6,10 +6,13 @@ import { partials } from './config/partialOptions.ts';
 import { onReady } from './src/events/onReady.ts';
 import { errorHandler } from './src/utils/errorHandler.ts';
 import { onInteraction } from './src/events/onInteraction.ts';
+import { connectDatabase } from '@orsted/commands';
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
     const client = new Client({ intents: intents, partials: partials });
+
+    await connectDatabase();
 
     client.on('error', errorHandler);
     client.once(Events.ClientReady, onReady);
