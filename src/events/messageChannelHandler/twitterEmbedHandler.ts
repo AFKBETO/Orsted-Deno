6de6b8c3066646b2ce2a3c26e4572dbd7ab02f58
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-04-22 17:31:09 +0200

    feat: add twitter link handling

---
+++ src/events/messageChannelHandler/twitterEmbedHandler.ts
@@ -0,0 +1,30 @@
+import { Message } from 'discord.js';
+const twitterRegex =
+    /(http(s):\/\/)(www\.){0,1}(x|twitter)\.com\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g;
+export async function twitterEmbedHandler(message: Message): Promise<void> {
+    try {
+        const { client } = message;
+        if (!client.botConfig) {
+            throw new Error('Client botConfig is not initialized');
+        }
+        const twitterEmbedLinks = client.botConfig.twitterEmbedLinks;
+        if (!twitterEmbedLinks) {
+            throw new Error('Twitter embed links not found in bot config');
+        }
+        const messageContent = message.content;
+        const twitterLink = messageContent.match(twitterRegex);
+        if (!twitterLink) {
+            return;
+        }
+        const twitterLinkString = twitterLink[0];
+        const twitterEmbedLink = twitterEmbedLinks.randomItem();
+        const newLink = twitterLinkString.replace(
+            /(www\.){0,1}(x|twitter)\.com/,
+            twitterEmbedLink,
+        );
+        await message.reply({ content: newLink });
+        await message.suppressEmbeds(true);
+    } catch (error) {
+        console.error(new Date(), 'Error in twitterEmbedHandler:', error);
+    }
+}
--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -1,5 +1,6 @@
 import { Message } from 'discord.js';
 import { spoilerThreadHandler } from './messageChannelHandler/spoilerThreadHandler.ts';
+import { twitterEmbedHandler } from './messageChannelHandler/twitterEmbedHandler.ts';
 
 const spoilerThreadId = '1156303744670105661';
 
@@ -7,7 +8,12 @@ export async function onMessageCreate(message: Message): Promise<void> {
     try {
         if (message.channelId === spoilerThreadId) {
             await spoilerThreadHandler(message);
+            return;
         }
+        if (message.author.bot) {
+            return;
+        }
+        await twitterEmbedHandler(message);
     } catch (error) {
         console.error('Error handling message:', error);
     }
